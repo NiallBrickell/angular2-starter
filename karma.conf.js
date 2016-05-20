a01ea73ab6d9facf498cc3f68bb0276bc69887ce
Niall Brickell <niallbrickell@gmail.com>
2016-05-20 08:38:24 +0100

    Developing tests to work with new structure

--- karma.conf.js
+++ karma.conf.js
@@ -6,13 +6,13 @@ module.exports = function(config) {
   var configuration = {
     basePath: '',
 
-    frameworks: ['jasmine'],
+    frameworks: ['jspm', 'jasmine'],
     browsers: ['PhantomJS'],
     reporters: ['progress', 'coverage'],
 
     preprocessors: {
-      'tmp/app/**/!(*.spec)+(.js)': ['coverage'],
-      'tmp/app/**/*.js': ['sourcemap']
+      'build_dev/app/**/!(*.spec)+(.js)': ['coverage'],
+      'build_dev/app/**/*.js': ['sourcemap']
     },
 
     // Generate json used for remap-istanbul
@@ -23,58 +23,68 @@ module.exports = function(config) {
       ]
     },
 
-    files: [
-      'node_modules/traceur/bin/traceur-runtime.js',
-      // IE required polyfills, in this exact order
-      'node_modules/es6-shim/es6-shim.min.js',
-      'node_modules/systemjs/dist/system-polyfills.js',
-      'node_modules/zone.js/dist/zone.js',
-      'node_modules/reflect-metadata/Reflect.js',
-      'node_modules/zone.js/dist/async-test.js',
-      'node_modules/zone.js/dist/fake-async-test.js',
-      'node_modules/systemjs/dist/system.src.js',
+    jspm: {
+    	config: 'build_dev/config.js',
+    	packages: 'build_dev/jspm_packages',
+    	loadFiles: [
+    		'test_js/**/*.js',
+	    	'karma-test-shim.js'
+    	],
+    	sourceFiles: [
+    		'build_dev/**/*.js',
+    		'client/**/*.js'
+    		//'node_modules/**/*.js'
+    	]
+    },
 
-      'systemjs.conf.js',
-      'karma-test-shim.js',
-
-      { pattern: 'tmp/app/**/*.js', included: false },
-      { pattern: 'tmp/test/test-helpers/*.js', included: false },
-
-      // paths loaded via Angular's component compiler
-      // (these paths need to be rewritten, see proxies section)
-      { pattern: 'app/**/*.html', included: false },
-      { pattern: 'app/**/*.css', included: false },
-
-      // paths to support debugging with source maps in dev tools
-      { pattern: 'app/**/*.ts', included: false, watched: false },
-      { pattern: 'tmp/app/**/*.js.map', included: false, watched: false }
-    ],
+    // files: [
+    //   // paths to support debugging with source maps in dev tools
+    //   { pattern: 'client/**/*.ts', included: false, watched: false},
+    //   { pattern: 'client/**/*.js.map', included: false, watched: false}
+    // ],
 
     // proxied base paths
     proxies: {
       // required for component assests fetched by Angular's compiler
-      "/app/": "/base/app/",
-      "/tmp/app/": "/base/tmp/app/",
-      "/tmp/test/": "/base/tmp/test/",
-      "/node_modules/": "/base/node_modules/"
+      // rootOnLocal : /base/ServeLocation
+      // As Karma serves everything from base, so we need to map the dirs from . (hs/frontend/) to it's serve location (/base)
+      // "/build_dev/": "/base/", //TODO: WHY NOT WORKING
+      // "/build_dev/": "/base/client/", //NOTE: THIS might say /client/.. cannot be found - but it doesn't mean build_dev isn't mapping to client! Maybe try include a file at the root of build_dev / client in a test file and see if ALL the sub-dirs are proxies or just the files in the specified folder, ie:
+      // "/build_dev/app/": "/base/client/app/",
+      // "/build_dev/app/blocks/": "/base/client/app/blocks/",
+
+      "/base/jspm_packages/": "/build_dev/jspm_packages/",
+      // "/base/client/": "/client/", //TODO: Or other way around? (and above)
+      //with this? :
+      "/build_dev/": "/base/",
+      // "/base/client/": "/base/", //TODO: NEED ALIAS!! Can this be done with proxy?? Or is the error we're getting strange and it's not really there? YES!!!!! JS IS NOT IN CLIENT!!!! NEW!!!!!********* Need to alias client to jspm packages! should be /build_dev but doesn't seem to like two paths in proxy (old /client/: /base/client/. Does redirecting (if it is at all) /base/client to /base (/ the other way rouhd - need to figure out how it works) work?? Or does it just freeze it? Yes, it just freezes it. Does it work differently redirecting base to base/something? DOES THIS JUST CAUSE LOOPS??
+      //OR::
+      "/build_dev/app/": "/base/client/app" //DOES THIS JUST LOOP AND BREAK EVERYTHING? TRY SEE IF WE GET SOCKET ERRORS WITH IT TAKEN OUT!
+
+      // "/build_dev/app/": "/base/app/",
+      // "/build_dev/jspm_packages/": "/base/jspm_packages/"
+      // "/jspm_packages/": "/base/jspm_packages/",
+      // "/node_modules/": "/base/node_modules/",
+      // "/test_js/": "/base/test_js/",
+      // "client/": "build_dev/"
     },
 
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    singleRun: true,
+    singleRun: false, //DEV
   };
 
-  Object.keys(dependencies).forEach(function(key) {
-    if(excludedDependencies.indexOf(key) >= 0) { return; }
+  // Object.keys(dependencies).forEach(function(key) {
+  //   if(excludedDependencies.indexOf(key) >= 0) { return; }
 
-    configuration.files.push({
-        pattern: 'node_modules/' + key + '/**/*.js',
-        included: false,
-        watched: false
-    });
-  });
+  //   configuration.files.push({
+  //       pattern: 'node_modules/' + key + '/**/*.js',
+  //       included: false,
+  //       watched: false
+  //   });
+  // });
 
   if (process.env.APPVEYOR) {
     configuration.browsers = ['IE'];
