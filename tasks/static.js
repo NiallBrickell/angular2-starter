087ffd69ffba421cd677923e61b59c47fa88613a
Niall Brickell <niallbrickell@gmail.com>
2016-05-25 19:38:12 +0100

    Globals SCSS file with Bootstrap vars. Static only compiles changed files.

--- tasks/static.js
+++ tasks/static.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
 	config = require('../gulp.config')(),
-	changed = require('gulp-changed');
+	changed = require('gulp-changed'),
+	path = require('path');
 
 gulp.task('copyStatic', function() {
 	return gulp.src([config.client + '*.{html,js,map,json,css,woff,ttf,jpg,png,gif,ico}', config.client + '!(jspm_packages)/**/*.{html,js,map,json,css,woff,ttf,jpg,png,gif,ico}'], {base: config.client})
@@ -14,5 +15,14 @@ gulp.task('copyVendor', function() {
 gulp.task('static', ['copyStatic', 'copyVendor']);
 
 gulp.task('watch-static', function () {
-    gulp.watch([config.client + '*.{html,js,map,json,css,woff,ttf,jpg,png,gif,ico}', config.client + '!(jspm_packages)/**/*.{html,js,map,json,css,woff,ttf,jpg,png,gif,ico}'], ['static']);
+    gulp.watch('client/jspm_packages/**/*.{html,js,map,json,css,woff,ttf,jpg,png,gif,ico}', function (file) {
+        console.log('Copying vendor file ' + file.path + ' to '+(config.build_dev.path+file.path.replace(path.resolve('./client/'), '')).replace('//','/')+'...');
+        // Get relative path to output file in build_dir and pipe watched file to it
+        return gulp.src(file.path, {base: path.resolve(config.client)}).pipe(gulp.dest(config.build_dev.path));
+    });
+    gulp.watch([config.client + '*.{html,js,map,json,css,woff,ttf,jpg,png,gif,ico}', config.client + '!(jspm_packages)/**/*.{html,js,map,json,css,woff,ttf,jpg,png,gif,ico}'], function (file) {
+        console.log('Copying app file ' + file.path + ' to '+(config.build_dev.path+file.path.replace(path.resolve('./client/'), '')).replace('//','/')+'...');
+        // Get relative path to output file in build_dir and pipe watched file to it
+        return gulp.src(file.path, {base: path.resolve(config.client)}).pipe(gulp.dest(config.build_dev.path));
+    });
 });
